dc475fe4e131227038e208a134cd4e8b82990984
Paul Moelders <Paul_Moelders@yahoo.de>
2013-06-01 18:45:37 +0200

    prototype design change

---
+++ js/hoover.js
@@ -0,0 +1,11 @@
+// hoover.js
+var data = 
+{
+  title: 'Initial commit for hoover',
+  creator: 'PM',
+  date: '21.05.2013',
+  time: '11:30',
+  tags: '#meta #hoover',
+  link: 'http://google.de',
+  description: 'Go for it!'
+};
